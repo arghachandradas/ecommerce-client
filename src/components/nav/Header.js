d2f97984959f1eb6427dd22f0d872b4c5f47a71c
argha chandra das <arghachandra.das@gmail.com>
2021-02-27 13:29:30 +0530

    added navigation functionality for our app

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,25 +1,41 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Menu } from 'antd';
-import { MailOutlined, SettingOutlined } from '@ant-design/icons';
+import {
+    HomeOutlined,
+    SettingOutlined,
+    LoginOutlined,
+    UserAddOutlined,
+} from '@ant-design/icons';
 
-const { SubMenu } = Menu;
+const { SubMenu, Item } = Menu;
 
 const Header = () => {
     // eslint-disable-next-line
-    const [current, setCurrent] = useState('');
+    const [current, setCurrent] = useState('home');
 
     const handleClick = (e) => {
-        console.log(e.key);
+        setCurrent(e.key);
     };
 
     return (
         <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
-            <Menu.Item key="mail" icon={<MailOutlined />}>
-                Home
-            </Menu.Item>
-            <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Register">
-                <Menu.Item key="setting:1">Option 1</Menu.Item>
-                <Menu.Item key="setting:2">Option 2</Menu.Item>
+            <Item key="home" icon={<HomeOutlined />}>
+                <Link to="/">Home</Link>
+            </Item>
+            <Item
+                key="register"
+                icon={<UserAddOutlined />}
+                className="float-right"
+            >
+                <Link to="/register">Register</Link>
+            </Item>
+            <Item key="login" icon={<LoginOutlined />} className="float-right">
+                <Link to="/login">Login</Link>
+            </Item>
+            <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Username">
+                <Item key="setting:1">Option 1</Item>
+                <Item key="setting:2">Option 2</Item>
             </SubMenu>
         </Menu>
     );

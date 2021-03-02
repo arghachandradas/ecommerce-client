83976d990844e72233a8919303cf8b392babf8da
argha chandra das <arghachandra.das@gmail.com>
2021-03-02 11:26:46 +0530

    added LoadingToRedirect component that we redirect on accessing protected route with Timer

---
+++ src/components/routes/LoadingToRedirect.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+const LoadingToRedirect = () => {
+    const [count, setCount] = useState(5);
+    let history = useHistory();
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCount((currentCount) => --currentCount);
+        }, 1000);
+
+        // redirect once count = 0
+        count === 0 && history.push('/');
+        // cleanup
+        return () => clearInterval(interval);
+    }, [count]);
+    return (
+        <div className="container p-5 text-center">
+            <h4>Redirecting you in {count} seconds ...</h4>
+        </div>
+    );
+};
+
+export default LoadingToRedirect;
--- src/components/routes/UserRoute.js
+++ src/components/routes/UserRoute.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { Spin } from 'antd';
+import LoadingToRedirect from './LoadingToRedirect';
 
 const UserRoute = ({ children, ...rest }) => {
     const { user } = useSelector((state) => ({ ...state }));
@@ -10,9 +10,7 @@ const UserRoute = ({ children, ...rest }) => {
     return user && user.token ? (
         <Route {...rest} render={() => children} />
     ) : (
-        <div className="d-flex justify-content-center align-items-center">
-            <Spin size="large" />
-        </div>
+        <LoadingToRedirect />
     );
 };
 
